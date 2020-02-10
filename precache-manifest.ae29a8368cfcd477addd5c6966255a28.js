fee31c51778696f338699cf3b322a769e67a668c
navydrum <navydrum@gmail.com>
2020-02-10 15:20:48 +0900

    Updates

---
+++ precache-manifest.ae29a8368cfcd477addd5c6966255a28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe3187680af2633b83dbc3fc34a4162",
+    "url": "/lol/index.html"
+  },
+  {
+    "revision": "7722f4346b11dd95ab09",
+    "url": "/lol/static/css/main.490f8fc1.chunk.css"
+  },
+  {
+    "revision": "ce37c0a81b012520fc21",
+    "url": "/lol/static/js/2.d102b65f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/lol/static/js/2.d102b65f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7722f4346b11dd95ab09",
+    "url": "/lol/static/js/main.2ee8ad84.chunk.js"
+  },
+  {
+    "revision": "7323cfd64588c04d7231",
+    "url": "/lol/static/js/runtime-main.2597b635.js"
+  },
+  {
+    "revision": "38fb1e86b1f5004024f773b3fc05948d",
+    "url": "/lol/static/media/champion.38fb1e86.png"
+  },
+  {
+    "revision": "29ea49618cf9f4acfb153698df765f14",
+    "url": "/lol/static/media/search_btn.29ea4961.png"
+  }
+]);
